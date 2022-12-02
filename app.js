4cd8a7b4712fae6ff622b17f798f3343d2b119c8
Luismaldonado <75577709+larayam@users.noreply.github.com>
2022-12-02 11:07:38 -0300

    Carga inicial
    Cargas de prueba

---
+++ app.js
@@ -0,0 +1,87 @@
+//const y = new Date().getFullYear();
+//url: "https://apis.digital.gob.cl/fl/feriados/" + y,
+
+function cargarTable() {
+
+  
+    $.ajax({
+        type: 'POST',
+        crossDomain: true,
+        url: 'https://farmanet.minsal.cl/index.php/ws/getLocales/',
+        dataType: 'jsonp',
+        success: function (data) {
+            
+          console.log(data);
+
+            for (var i = 0; i < data.length; i++) {
+
+                feriados.push({ fecha: data[i].fecha });
+            }
+
+        }
+    });
+
+  $.ajax({
+    type: "POST",
+    url: 'https://apis.digital.gob.cl/fl/feriados/',
+    dataType: 'jsonp',
+    contentType: false,
+    async: false,
+    success: function (respuesta) {
+
+      let resuelto = JSON.parse(JSON.stringify(respuesta));
+
+       let htmlTodo = `
+          <table id="dataTables-feriados" class="table table-striped table-bordered table-hover" data-page-length="10">
+            <thead>
+              <tr>
+                <th class="text-center">Fecha</th>
+                <th class="text-center">Titulo</th>
+                <th class="text-center">Tipo</th>
+              </tr>
+            </thead>
+          <tbody>`;
+
+      for (let i = 0; i < resuelto.length; i++) {
+
+        
+        htmlTodo += `
+            <tr>
+              <td class="text-nowrap bd-highlight" style="width: 8rem;">${resuelto[i].fecha.split('-').reverse().join('-')}</td>
+              <td class="text-nowrap bd-highlight" style="width: 8rem;">${resuelto[i].nombre}</td>
+              <td class="text-nowrap bd-highlight" style="width: 8rem;">${resuelto[i].tipo}</td>
+            </tr>`;
+      }
+
+      htmlTodo += `
+            </tbody>
+            </table>`;
+
+      $(".table-feriados").html(htmlTodo);
+      RearmTableEM("dataTables-feriados");
+    },
+  });
+}
+
+function RearmTableEM(tabla) {
+
+  $('#' + tabla).dataTable({
+    paging: true,
+    lengthChange: true,
+    bFilter: true,
+    searching: true,
+    ordering: true,
+    info: true,
+    bInfo: true,
+    autoWidth: false,
+    responsive: true,
+    order: [1, "Desc"],
+  });
+};
+
+
+
+
+cargarTable();
+
+
